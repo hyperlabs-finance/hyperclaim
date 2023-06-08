a4686d485ade24e64d894f09e535c08987a391cf
brucejayasinghe <brucejay43@gmail.com>
2023-06-08 12:11:24 +0100

    Notes added to curator

--- src/Hyperclaim/HyperclaimVerifiersCurator.sol
+++ src/Hyperclaim/HyperclaimVerifiersCurator.sol
@@ -5,7 +5,90 @@ pragma solidity ^0.8.6;
 import 'openzeppelin-contracts/contracts/access/Ownable.sol';
 import '../Interface/IHyperclaimVerifiersCurator.sol';
 
+// #TODO
+
 // Curator manages the addition of verifiers to the verifiers registry via submission and voting
+contract TokenCuratedRegistry {
+
+    struct Submission {
+        address submitter; // Address of the submission creator
+        string data; // Data associated with the submission
+        uint256 deposit; // Number of tokens staked as deposit
+        uint256 voteCount; // Number of votes received
+        bool isAccepted; // Whether the submission is accepted or rejected
+    }
+
+    IERC20 public token; // IERC20 token used for voting
+    uint256 public submissionDeposit; // Minimum number of tokens required as a deposit for submission
+    uint256 public votingPeriod; // Duration of the voting period in seconds
+
+    Submission[] public submissions; // Array of all submissions
+    mapping(address => uint256) public balances; // Mapping of token balances for each participant
+    mapping(address => mapping(uint256 => bool)) public hasVoted; // Mapping to track whether a participant has voted on a submission
+
+    event SubmissionCreated(uint256 indexed submissionId, address indexed submitter, string data);
+    event SubmissionVoted(uint256 indexed submissionId, address indexed voter, uint256 voteCount);
+    event SubmissionAccepted(uint256 indexed submissionId);
+    event SubmissionRejected(uint256 indexed submissionId);
+
+    constructor(address _tokenAddress, uint256 _submissionDeposit, uint256 _votingPeriod) {
+        token = IERC20(_tokenAddress);
+        submissionDeposit = _submissionDeposit;
+        votingPeriod = _votingPeriod;
+    }
+
+    function submit(string memory _data) external {
+        require(token.balanceOf(msg.sender) >= submissionDeposit, "Insufficient tokens to submit");
+        require(token.transferFrom(msg.sender, address(this), submissionDeposit), "Token transfer failed");
+
+        Submission memory newSubmission = Submission({
+            submitter: msg.sender,
+            data: _data,
+            deposit: submissionDeposit,
+            voteCount: 0,
+            isAccepted: false
+        });
+        uint256 submissionId = submissions.length;
+        submissions.push(newSubmission);
+
+        emit SubmissionCreated(submissionId, msg.sender, _data);
+    }
+
+    function vote(uint256 _submissionId, uint256 _voteCount) external {
+        require(_submissionId < submissions.length, "Invalid submission ID");
+        require(!hasVoted[msg.sender][_submissionId], "Already voted on this submission");
+
+        Submission storage submission = submissions[_submissionId];
+        require(submission.isAccepted == false, "Submission has already been accepted");
+
+        hasVoted[msg.sender][_submissionId] = true;
+        submission.voteCount += _voteCount;
+        balances[msg.sender] += _voteCount;
+
+        emit SubmissionVoted(_submissionId, msg.sender, _voteCount);
+    }
+
+    function processSubmission(uint256 _submissionId) external {
+        require(_submissionId < submissions.length, "Invalid submission ID");
+
+        Submission storage submission = submissions[_submissionId];
+        require(submission.isAccepted == false, "Submission has already been processed");
+
+        // Check if the voting period has ended
+        require(block.timestamp >= votingPeriod, "Voting period has not ended yet");
+
+        if (submission.voteCount > (submissions.length / 2)) {
+            submission.isAccepted = true;
+            emit SubmissionAccepted(_submissionId);
+        } else {
+            // If the submission is rejected, return the staked deposit to the submitter
+            require(token.transfer(submission.submitter, submission.deposit), "Token transfer failed");
+            emit
+
+ SubmissionRejected(_submissionId);
+        }
+    }
+}
 
 contract HyperclaimVerifiersCurator is IHyperclaimVerifiersCurator, Ownable {
 
